7a71b55dc1eb773d99e0f4b8e5d07b79058022da
tongshiyang <tongshiyang@126.com>
2020-01-03 09:52:05 +0800

    first commit

---
+++ eureka_client/src/test/java/com/example/eureka_client/eureka_client/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_client.eureka_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka_server/src/test/java/com/example/eureka_server/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_server.eureka_server;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
